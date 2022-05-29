24c705df0b1e0b94e031792e5e8d6295b681a97b
Amy-Dai <happydaimeiyin@126.com>
2022-05-29 18:18:16 +0800

    week05

--- week04/concurrency/src/main/java/com/example/concurrency/Homework.java
+++ week04/concurrency/src/main/java/com/example/concurrency/Homework.java
@@ -1,5 +1,8 @@
 package com.example.concurrency;
 
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
 public class Homework {
     public static void main(String[] args) {
 
@@ -7,15 +10,57 @@ public class Homework {
 
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
+        Callable<Integer> callable = new Callable<>() {
+            @Override
+            public Integer call() throws Exception {
+                return sum();
+            }
+        };
+        FutureTask<Integer> func1 = new FutureTask<>(callable);
+        new Thread(func1).start();
+        int result = 0;
+        try {
+            result = func1.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        int result = sum(); //这是得到的返回值
+//        int result = sum(); //这是得到的返回值
 
         // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
+        System.out.println("Method-1：FutureTask异步计算结果为："+result);
+        System.out.println("Method-1：FutureTask使用时间："+ (System.currentTimeMillis()-start) + " ms");
 
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+        start = System.currentTimeMillis();
+        ExecutorService threadPool = Executors.newFixedThreadPool(1);
+        Future<Integer> poolFuture = threadPool.submit(callable);
+        try {
+            result = poolFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // 确保  拿到result 并输出
+        System.out.println("Method-2，Threadpool Future异步计算结果为："+result);
+        System.out.println("Method-2，Threadpool Future使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        start = System.currentTimeMillis();
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return sum();
+            }
+        }, threadPool);
+        try {
+            completableFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Method-3，CompletableFuture异步计算结果为："+result);
+        System.out.println("Method-3，CompletableFuture使用时间："+ (System.currentTimeMillis()-start) + " ms");
 
         // 然后退出main线程
+        System.exit(0);
     }
 
     private static int sum() {
