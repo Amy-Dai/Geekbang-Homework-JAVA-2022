5d86fb2228792eea57ef67834be9297e75f6dadc
Amy-Dai <happydaimeiyin@126.com>
2022-06-05 23:22:32 +0800

    week05

--- week05/demo/src/main/java/com/example/demo/spring/Main.java
+++ week05/demo/src/main/java/com/example/demo/spring/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("resources/configurations/knights.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("configurations/knights.xml");
         IKnight knight = context.getBean(IKnight.class);
         knight.embarkOnQuest();
         context.close();
